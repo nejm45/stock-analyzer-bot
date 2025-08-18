eb8e8f01a0d3e37f55a72b43acb073053593d73f
nejm45 <68993909+nejm45@users.noreply.github.com>
2025-08-18 06:39:54 +0000

    jeu word binance n1

---
+++ word-of-the-day.py
@@ -0,0 +1,37 @@
+from nltk.corpus import words
+import nltk
+
+# تحميل قاموس الكلمات (مرة واحدة فقط)
+nltk.download('words')
+
+# الحصول على كلمات إنجليزية مكونة من 6 حروف
+word_list = [word.lower() for word in words.words() if len(word) == 4]
+
+# الشروط
+required_letters = {'a', 'p'}
+excluded_letters = {'s', 'f', 'e', 'c', 'k', 't', 'm', 'w', 'r'}
+
+def is_valid(word):
+    return (
+        required_letters.issubset(set(word)) and
+        word[1] == 'a' and            # الحرف الثالث = c
+        word[0] != 'p' and 
+        #word[3] != 's' and 
+        #word[0] != 'a' and            # الحرف الثاني ≠ e
+       word[3] == 'p' and
+       #word[2] == 'a' and 
+       #word[2] == 'c' and 
+       #word[4] == 's' and  
+        #word.count('t') == 1 and      # e لا يتكرر مرتين
+        #word.count('o') == 1 and 
+        # word[4] != 'r' and            # الحرف الخامس ≠ r
+        
+        
+        not any(letter in excluded_letters for letter in word)
+    )
+
+# تطبيق الشروط
+valid_words = [word for word in word_list if is_valid(word)]
+
+# عرض النتائج
+print(valid_words[:20])  # عرض أول 20 كلمة مطابقة إن وُجدت
