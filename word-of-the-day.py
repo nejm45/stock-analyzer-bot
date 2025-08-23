8db5cbc442b07df2d2c79dcf803e87b8b051df50
nejm45 <68993909+nejm45@users.noreply.github.com>
2025-08-23 18:24:08 +0200

    Update word-of-the-day.py
    zk

--- word-of-the-day.py
+++ word-of-the-day.py
@@ -5,20 +5,20 @@ import nltk
 nltk.download('words')
 
 # الحصول على كلمات إنجليزية مكونة من 6 حروف
-word_list = [word.lower() for word in words.words() if len(word) == 4]
+word_list = [word.lower() for word in words.words() if len(word) == 5]
 
 # الشروط
-required_letters = {'a', 'p'}
-excluded_letters = {'s', 'f', 'e', 'c', 'k', 't', 'm', 'w', 'r'}
+required_letters = {'t', 'e'}
+excluded_letters = {'o', 'k', 'n'}
 
 def is_valid(word):
     return (
         required_letters.issubset(set(word)) and
-        word[1] == 'a' and            # الحرف الثالث = c
-        word[0] != 'p' and 
+        word[3] == 'e' and            # الحرف الثالث = c
+        word[0] != 't' and 
         #word[3] != 's' and 
         #word[0] != 'a' and            # الحرف الثاني ≠ e
-       word[3] == 'p' and
+       #word[3] == 'p' and
        #word[2] == 'a' and 
        #word[2] == 'c' and 
        #word[4] == 's' and  
